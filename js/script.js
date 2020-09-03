54a50baa32405046f1bc53d3be952f4a0b5c15bf
nikitka <nick.urbanovich@mail.ru>
2020-09-03 13:07:04 +0300

    delete array result

--- js/script.js
+++ js/script.js
@@ -7,7 +7,6 @@ $(function () {
         $(this).prop('disabled',true)
         randomNumberBtn.prop('disabled',false)
         let cols = $('.wrap .col')
-        let result = []
         cols.each(function () {
             let n = Math.floor(Math.random() * 90) + 1
             $(this).html(n)
