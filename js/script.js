3385f6783e2645452629fcbfce32b1d382b06a03
nikitka <nick.urbanovich@mail.ru>
2020-09-03 21:22:49 +0300

    aa

--- js/script.js
+++ js/script.js
@@ -3,27 +3,30 @@ $(function () {
     let startGameBtn = $('#startGeneration')//кнопка генерирования случайных чисел в лотерейном билете
     let randomNumberBtn = $('#randomNumber')//кнопка для зачеркивания номера в билете
     let cols = $('.wrap .col')
+    let arr = []
+    while (arr.length < 30){
+        let randomNumber = Math.floor(Math.random() * 90) + 1
+        if (arr.indexOf(randomNumber) === -1){
+            arr.push(randomNumber)
+        }
+    }
     randomNumberBtn.prop('disabled',true)
     startGameBtn.click(function () {
         $(this).prop('disabled',true)
         randomNumberBtn.prop('disabled',false)
-        let arr = []
-        while (arr.length < 30){
-            let randomNumber = Math.floor(Math.random() * 90) + 1
-            if (arr.indexOf(randomNumber) === -1){
-                arr.push(randomNumber)
-            }
-        }
         cols.each(function (index) {
             $(this).html('<div class="pos">' + arr[index] + '</div>')
         })
 
     })
+
     randomNumberBtn.click(function () {
+        let $cols = $('.wrap .col .pos')
+        let arr = []
+        $cols.each(function () {
+            arr.push(Number($(this).html()))
+        })
         let randomNumber = Math.floor(Math.random() * 90) + 1
 
     })
-
-
-
 })
